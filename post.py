e2f816c9051d77744c83218b158a2d903f63685d
Sibgatov Damir <77218926+damir-kalin@users.noreply.github.com>
2023-12-21 17:17:30 +0300

    Refactoring 21.12

--- post.py
+++ post.py
@@ -5,19 +5,19 @@ import os
 
 
 
-# url = 'http://localhost:80/parse'
-# headers = {'Content-type': 'application/json',
-#            'Accept': 'text/plain',
-#            'Content-Encoding': 'utf-8'}
-# data = {'profession':'Аналитик'
-#         ,'city_id':'1'
-#         ,'date': str(date.today() - timedelta(days=8))
-#         }
+url = 'http://localhost:80/parse'
+headers = {'Content-type': 'application/json',
+           'Accept': 'text/plain',
+           'Content-Encoding': 'utf-8'}
+data = {'profession':'Аналитик'
+        ,'city_id':'1'
+        ,'date': str(date.today() - timedelta(days=8))
+        }
 
-# print(data)
+print(data)
 
-# answer = requests.post(url, headers=headers, json=data)
+answer = requests.post(url, headers=headers, json=data)
 
-# print(answer.headers, answer.status_code, answer.text) 
-with open('.env', 'r', encoding='utf-8') as f:
-        print(f.readlines())
+print(answer.headers, answer.status_code, answer.text) 
+# with open('.env', 'r', encoding='utf-8') as f:
+#         print(f.readlines())
